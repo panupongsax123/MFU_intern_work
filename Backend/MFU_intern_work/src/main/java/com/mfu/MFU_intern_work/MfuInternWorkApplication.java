86eac965a3d55e73789a713d27532853e320ca47
TheBeatles1960 <patong123_12@hotmail.com>
2020-04-03 17:21:06 +0700

    no message

---
+++ Backend/MFU_intern_work/src/main/java/com/mfu/MFU_intern_work/MfuInternWorkApplication.java
@@ -0,0 +1,13 @@
+package com.mfu.MFU_intern_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfuInternWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfuInternWorkApplication.class, args);
+	}
+
+}
