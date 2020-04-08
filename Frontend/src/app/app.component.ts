ea2101afa72d3878cd624e68b258d5a4aa8f6780
saxiooo <6031501043@lamduan.mfu.ac.th>
2020-04-08 12:33:12 +0700

    update button

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internshipwork';
+
+  constructor(private router: Router) { }
+
+  List() {
+    // console.log("List Book");
+    this.router.navigate(["list"]);
+  }
 }
